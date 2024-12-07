78cafe0d0135865794f62553fbb3c39b83cd5c06
Jackiee2002 <hoangbuu1511@gmail.com>
2024-12-07 03:34:43 -0500

    Create the Search function and Tag function

--- frontend/src/app/components/pages/home/home.component.ts
+++ frontend/src/app/components/pages/home/home.component.ts
@@ -2,23 +2,35 @@ import { Component, OnInit } from '@angular/core';
 import { FoodService } from '../../../services/food.service';
 import { Food } from '../../../shared/models/Food';
 import { CommonModule } from '@angular/common';
-import { RouterLink } from '@angular/router';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import { SearchComponent } from '../../partials/search/search.component';
+import { TagsComponent } from '../../partials/tags/tags.component';
 
 
 @Component({
   selector: 'app-home',
   imports: [
-CommonModule,
-RouterLink,
-  ],
+    CommonModule,
+    RouterLink,
+    SearchComponent,
+    TagsComponent
+],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
 
   foods:Food[] = [];
-  constructor(private foodService:FoodService) {
-    this.foods = foodService.getAll();
+  constructor(private foodService:FoodService, activatedRoute:ActivatedRoute) {
+    activatedRoute.params.subscribe((params) => {
+      if(params.searchTerm)
+        this.foods = this.foodService.getAllFoodsBySearchTerm(params.searchTerm);
+      else if (params.tag)
+        this.foods = this.foodService.getAllFoodsByTag(params.tag)
+      else
+        this.foods = foodService.getAll();
+    })
+
   }
 
   ngOnInit(): void {
---
+++ frontend/src/app/components/partials/tags/tags.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { FoodService } from '../../../services/food.service';
+import { Tag } from '../../../shared/models/Tag';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-tags',
+  imports: [
+    CommonModule,
+    RouterLink,
+  ],
+  templateUrl: './tags.component.html',
+  styleUrls: ['./tags.component.css']
+})
+export class TagsComponent implements OnInit {
+  tags?:Tag[];
+  constructor(foodService:FoodService) {
+    this.tags = foodService.getAllTags();
+   }
+
+  ngOnInit(): void {
+  }
+
+}
