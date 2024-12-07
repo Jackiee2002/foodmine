1684a42c3124938d7b78d55667101e52da8dc968
Jackiee2002 <hoangbuu1511@gmail.com>
2024-12-07 16:51:56 -0500

    add a food detail page and add to cart function

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -8,11 +8,15 @@ import { RatingModule } from 'ng-starrating';
 import { CommonModule } from '@angular/common';
 import { SearchComponent } from './components/partials/search/search.component';
 import { TagsComponent } from './components/partials/tags/tags.component';
+import { FoodPageComponent } from './components/pages/food-page/food-page.component';
+import { CartPageComponent } from './components/pages/cart-page/cart-page.component';
 
 @NgModule({
   
   declarations: [
     AppComponent,
+    FoodPageComponent,
+    CartPageComponent,
     
     
     
